28184910f21bda07f9ac2849ac2ae82ccc4e7c2c
KD131 <71063280+KD131@users.noreply.github.com>
2024-01-08 13:42:04 +0100

    user entity

---
+++ src/main/java/org/example/notificationservice/entity/User.java
@@ -0,0 +1,26 @@
+package org.example.notificationservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(name = "email", nullable = false)
+    private String email;
+}
