f233c907eeabd2a2c4e3b86494fbceb323868d8f
KD131 <71063280+KD131@users.noreply.github.com>
2024-01-08 14:10:34 +0100

    user table migration

---
+++ src/main/resources/db/migration/V1__user_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE user
+(
+    id         INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name  VARCHAR(255) NOT NULL,
+    email      VARCHAR(255) NOT NULL,
+    CONSTRAINT pk_user PRIMARY KEY (id)
+);
